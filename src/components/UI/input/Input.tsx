86548b3183bdad5cdc42663483afb73c2d11839c
Libor <libor26@gmail.com>
2024-08-29 02:47:11 +0200

    Input UI component with styling and model

---
+++ src/components/UI/input/Input.tsx
@@ -0,0 +1,46 @@
+/** LIBRARIES */
+import { ChangeEvent, FC } from "react";
+
+/** MODELS */
+import { FlexDirections, FlexWrap, InputStyles, InputTypes } from "../../../models/input";
+import { LinkLabels } from "../../../models/link";
+
+/** STYLES */
+import styles from "./Input.module.css";
+
+interface InputContainerProps {
+  direction?: FlexDirections;
+  wrap?: boolean;
+}
+
+interface InputFieldProps {
+  className: InputStyles;
+  id: string;
+  label?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: LinkLabels;
+  type?: InputTypes;
+}
+
+interface InputProps {
+  containerProps: InputContainerProps;
+  fieldProps: InputFieldProps;
+}
+
+const Input: FC<InputProps> = ({ containerProps, fieldProps }) => {
+  const { className, id, label, type = InputTypes.SEARCH } = fieldProps;
+  const { direction = FlexDirections.ROW, wrap = true } = containerProps;
+
+  const flexWrap = wrap ? FlexWrap.WRAP : FlexWrap.NOWRAP;
+  const containerClasses = `${styles.container} ${styles[direction]} ${styles[flexWrap]}`;
+  const inputClasses = `${styles.input} ${styles[className]}`;
+
+  return (
+    <div className={containerClasses}>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input {...fieldProps} className={inputClasses} type={type} />
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/models/input.ts
@@ -0,0 +1,22 @@
+export enum FlexDirections {
+  COLUMN = "column",
+  ROW_REVERSE = "row-reverse",
+  ROW = "row",
+}
+
+export enum FlexWrap {
+  NOWRAP = "nowrap",
+  WRAP = "wrap",
+}
+
+export enum InputStyles {
+  SEARCH = "search",
+}
+
+export enum InputTypes {
+  CHECKBOX = "checkbox",
+  NUMBER = "number",
+  RADIO = "radio",
+  SEARCH = "search",
+  TEXT = "text",
+}
